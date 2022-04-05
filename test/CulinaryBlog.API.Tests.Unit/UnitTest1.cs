576e56d2e5516eea479470e05b06291c2afc82c2
Krzysztof Sobieraj <krzysiek@sobieraj.mobi>
2022-04-05 22:44:02 +0200

    add unit tests projects

---
+++ test/CulinaryBlog.API.Tests.Unit/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace CulinaryBlog.API.Tests.Unit;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
---
+++ test/CulinaryBlog.Infrastructure.Tests.Unit/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace CulinaryBlog.Infrastructure.Tests.Unit;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
