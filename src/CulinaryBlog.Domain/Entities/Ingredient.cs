e8b59ae02b64f6c1f824ac57cf6805711ab95bf2
Krzysztof Sobieraj <krzysiek@sobieraj.mobi>
2022-04-06 22:26:19 +0200

    add unt tests for entities, update Domain tests namespace, initialize Guid in entities

---
+++ src/CulinaryBlog.Domain/Entities/Ingredient.cs
@@ -0,0 +1,9 @@
+namespace CulinaryBlog.Domain.Entities;
+
+public class Ingredient
+{
+    public Guid UUID { get; } = Guid.NewGuid();
+    public string Name { get; set; }
+
+    public IngredientCategory IngredientCategory { get; set; } = new IngredientCategory();
+}
---
+++ src/CulinaryBlog.Domain/Entities/IngredientCategory.cs
@@ -0,0 +1,7 @@
+namespace CulinaryBlog.Domain.Entities;
+
+public class IngredientCategory
+{
+    public Guid UUID { get; } = Guid.NewGuid();
+    public string Name { get; set; }
+}
